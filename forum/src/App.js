130106467838ea3a2cc892b043054212eaf2df21
Cyprien BELLEMIN MENARD <belleminmenardcyprien@hotmail.fr>
2022-07-20 16:43:34 +0200

    Adding register to app.js

--- forum/src/App.js
+++ forum/src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Page/Login/Login';
+import Register from './Page/Register/Register';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigate to='/login' />} />
       <Route path='/login' element={<Login />} />
+      <Route path='register' element={<Register />} />
     </Routes>
   );
 }
